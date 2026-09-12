1f1df00bc281360c61d456639fddfe4c1abf4f0e
dev07119 <devashishjangid11@gmail.com>
2026-09-13 03:20:06 +0530

    name chng

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 
 resource "google_compute_network" "poc_vpc" {
-  name                    = "spacelift-poc-vpc"
+  name                    = "spacelift-poc-vpcs"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "poc_subnet" {
-  name          = "spacelift-poc-subnet"
+  name          = "spacelift-poc-subnets"
   ip_cidr_range = "10.10.0.0/24"
   region        = var.region
   network       = google_compute_network.poc_vpc.id
