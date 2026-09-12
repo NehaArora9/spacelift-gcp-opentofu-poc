3db74e39052aa0e2a2c46a456d3557ed5a9db48e
dev07119 <devashishjangid11@gmail.com>
2026-09-13 03:22:11 +0530

    cidr

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_compute_network" "poc_vpc" {
 
 resource "google_compute_subnetwork" "poc_subnet" {
   name          = "spacelift-poc-subnets"
-  ip_cidr_range = "10.10.0.0/24"
+  ip_cidr_range = "10.11.0.0/24"
   region        = var.region
   network       = google_compute_network.poc_vpc.id
  
