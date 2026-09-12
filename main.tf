6334bc97827b6a3281dacaa68877a603bf2b9baa
dev07119 <devashishjangid11@gmail.com>
2026-09-12 19:34:40 +0530

    Add OpenTofu GCP infrastructure

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network" "poc_vpc" {
+  name                    = "spacelift-poc-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "poc_subnet" {
+  name          = "spacelift-poc-subnet"
+  ip_cidr_range = "10.10.0.0/24"
+  region        = var.region
+  network       = google_compute_network.poc_vpc.id
+}
