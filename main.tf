bf52dc75d12131e58d89d6fb2f07b46fb4d05f1a
dev07119 <devashishjangid11@gmail.com>
2026-09-14 00:10:55 +0530

    add http firewall rule

--- main.tf
+++ main.tf
@@ -9,5 +9,19 @@ resource "google_compute_subnetwork" "poc_subnet" {
   ip_cidr_range = "10.11.0.0/24"
   region        = var.region
   network       = google_compute_network.poc_vpc.id
- 
+
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = google_compute_network.poc_vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags = ["http-server"]
 }
